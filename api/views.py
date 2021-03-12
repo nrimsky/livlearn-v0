962ce09794f8ee993883fedc382bc4f3e5a727ed
Nina <ninarimsky@gmail.com>
2021-03-12 01:05:41 +0000

    basic django login logout

--- api/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from api.serialisers import UserSerialiser
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerialiser
