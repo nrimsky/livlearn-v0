367728e558f5b8019d445851dff16a1715552583
Nina <ninarimsky@gmail.com>
2021-03-17 19:08:31 +0000

    improve password reset and add user profile model

--- authapp/signals.py
+++ authapp/signals.py
@@ -1,10 +1,10 @@
-# from django.db.models.signals import post_save
-# from django.contrib.auth.models import User
-# from django.dispatch import receiver
-#
-#
-# @receiver(post_save, sender=User)
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         profile = UserProfile.objects.create(user=instance)
-#         profile.save()
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile.objects.create(user=instance)
+        profile.save()
