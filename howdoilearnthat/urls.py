df3e3021f0abbcfce8b2ac28716bf131579e355a
Nina <ninarimsky@gmail.com>
2021-03-21 19:15:05 +0000

    add pretty 404 page

--- howdoilearnthat/urls.py
+++ howdoilearnthat/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.defaults import page_not_found
 
 urlpatterns = [
+    path('404/', page_not_found, {'exception': Exception()}),
     path('me/', include('allauth.urls')),
     path('me/', include('authapp.urls', namespace='authapp')),
     path('', include('links.urls', namespace='links')),
